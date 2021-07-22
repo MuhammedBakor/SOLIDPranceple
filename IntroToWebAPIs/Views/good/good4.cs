689df1ce0259fcf599c263f79a472905499bc006
Muahmmed <assaa1122a@gmail.com>
2021-07-22 21:37:56 +0300

    add class4

---
+++ IntroToWebAPIs/Views/good/good4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroToWebAPIs.Views.good
+{
+    public class good4
+    {
+        public good4()
+        {
+
+            Console.WriteLine("Hi");
+        }
+    }
+}
