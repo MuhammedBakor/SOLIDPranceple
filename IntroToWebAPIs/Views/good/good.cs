bccb4e1a4030a60a288b0a6c690e1f853d6a7c05
Muahmmed <assaa1122a@gmail.com>
2021-07-22 20:27:50 +0300

    add new folder

---
+++ IntroToWebAPIs/Views/good/good.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroToWebAPIs.Views.good
+{
+    public class good
+    {
+        public good()
+        {
+
+            Console.WriteLine("Hi");
+        }
+    }
+}
